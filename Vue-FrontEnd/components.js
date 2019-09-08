2b75579c14ea901be06a108729dd89fa2228fc5c
Kristian Bonitz <kristian@bonitz.me>
2019-09-08 17:41:39 +1000

    Fixing everything
    Timer now actually works, 
    All the components are now sitting under a single application, dividing my files into components and application

---
+++ Vue-FrontEnd/components.js
@@ -0,0 +1,74 @@
+
+Vue.component('activity-clock', {
+    props: ['time'],
+    template: "<h2>{{ time.hour ? time.hour.toString().padStart(2, '0') + ':' : '' }}{{ time.minute.toString().padStart(2, '0') }}:{{ time.second.toString().padStart(2, '0') }}</h2>"
+});
+
+Vue.component('skip-button', {
+    template: "<button>Skip</button>"
+});
+Vue.component('restart-button', {
+    template: "<button>Restart</button>"
+});
+Vue.component('pauseplay-button', {
+    template: "<button>Pause/Play</button>"
+});
+
+Vue.component('activity-title', {
+    props: ['activity'],
+    template: "<div><h2>{{ activity.name }}</h2>\n" +
+              "<span>Using: {{ activity.objects.count }} {{ activity.objects.name }}</span>\n" +
+              "<div><span>Tags: </span>\n" +
+              "<ul>\n" +
+              "<li v-for='tag in activity.tags'>{{ tag }}</li>\n" +
+              "</ul></div></div>\n"
+});
+
+Vue.component('activity-timer-display',{
+    props:['time', 'activity'],
+    template: 
+        "<div id='activity-timer'><activity-title\n" + 
+        "v-bind:activity='activity'\n" +
+        "v-bind:key='activity.id'>\n"  +
+        "</activity-title>\n" +
+        "<activity-clock\n" + 
+        "v-bind:time='time'>\n"+
+        "</activity-clock></div>"
+});
+
+
+Vue.component('activity-timer-controls',{
+    props: ['inProgress','isRunning'],
+    template: 
+        "<div>Timer Controls\n" +
+        "<button v-on:click=\"$emit('skip')\">Skip</button>\n" +
+        "<button v-on:click=\"$emit('restart')\">Restart</button>\n" +
+        "<button \n" +
+        "v-if='!inProgress'\n" +
+        "v-on:click=\'$emit(\"start\")\'>Start</button>\n" +
+        "<button \n" +
+        "v-if='inProgress'\n" +
+        "v-on:click=\'$emit(\"stop\")\'>Stop</button>\n" +
+        "<button \n" +
+        "v-if='!isRunning'\n" +
+        "v-on:click=\'$emit(\"pause\")\'>Pause</button>\n" +
+        "<button \n" +
+        "v-if='isRunning'\n" +
+        "v-on:click=\'$emit(\"play\")\'>Play</button>\n"+
+        "</div>"
+});
+
+Vue.component('activity-list',{
+    props:['activityList'],
+    template: "<ol><activity-item \n" +
+        "v-for='activity in activityList' \n" +
+        "v-bind:activity='activity'\n" +
+        "v-bind:key='activity.id'></activity-item>\n" +
+        "</ol>"
+});
+
+Vue.component('activity-item', {
+    props: ['activity'],
+    template: "<li>{{ activity.objects.count }} {{ activity.objects.name }} {{ activity.name }}</li>" 
+    //TODO: add time length for {{ getTime(activity.learningStage) }}
+});
--- Vue-FrontEnd/script.js
+++ Vue-FrontEnd/script.js
@@ -1,89 +1,12 @@
 //TODO: How to use outside API for activity list
 
-function StartClock(){
-  clock = window.setInterval(checkTime, 1000);
-  drillsActive = true;
-  updateTimer();
-}
-
-Vue.component('activity-title', {
-    props: ['activity'],
-    template: "<div><h2>{{ activity.name }}</h2>\n" +
-              "<span>Using: {{ activity.objects.count }} {{ activity.objects.name }}</span>\n" +
-              "<div><span>Tags: </span>\n" +
-              "<ul>\n" +
-              "<li v-for='tag in activity.tags'>{{ tag }}</li>\n" +
-              "</ul></div></div>\n"
-});
-
-Vue.component('activity-clock', {
-    props: ['time'],
-    template: "<h2>{{ time.hour ? time.hour.toString().padStart(2, '0') + ':' : '' }}{{ time.minute.toString().padStart(2, '0') }}:{{ time.second.toString().padStart(2, '0') }}</h2>"
-});
-
-Vue.component('activity-timer-display',{
-    props:['time', 'activity'],
-    template:   "<div id='activity-timer'><activity-title" + 
-                "v-bind:activity='activity'\n" +
-                "v-bind:key='activity.id'>\n"  +
-                "</activity-title>\n" +
-                "<activity-clock\n" + 
-                "v-bind:time='time'>\n"+
-                "</activity-clock></div>\n"
-});
-
-Vue.component('activity-item', {
-    props: ['activity'],
-    template: "<li>{{ activity.objects.count }} {{ activity.objects.name }} {{ activity.name }}</li>" 
-    //TODO: add time length for {{ getTime(activity.learningStage) }}
-});
-
-Vue.component('skip-button', {
-    template: "<button>Skip</button>"
-});
-Vue.component('restart-button', {
-    template: "<button>Restart</button>"
-});
-Vue.component('pauseplay-button', {
-    template: "<button>Pause/Play</button>"
-});
-var activityTimerControls = new Vue({
-    el:'#activity-timer-controls',
-    data:{
-        clock: {},
-        isActive: false
-    },
-    methods: {
-        startTime: function(){
-            this.clock = window.setInterval(this.updateTime, 1000);
-            this.isActive = true;
-        },
-        stopTime: function(){
-            clearInterval(this.clock);
-            this.isActive = false;
-        },
-        updateTime: function(){
-            if(this.time.hour <= 0 && this.time.minute <= 0 && this.time.second <= 0){
-                clearInterval(this.clock);
-                //TODO: add start next activity if others exist or stop everything
-            }
-            if(this.time.second > 0){
-                this.time.second--;
-            }else if(this.time.minute > 0){
-                this.time.minute--;
-                this.time.second = 59;
-            }else if(this.time.hour > 0){
-                this.time.hour--;
-                this.time.minute = 59;
-                this.time.second = 59;
-            }
-        }
-    }
-})
-
-var activityList = new Vue({
-    el: '#activity-list',
+var app = new Vue({
+    el: '#app',
     data: {
+        time: {hour: 0, minute: 2, second: 0},
+        isRunning: false,
+        inProgress: false,
+        activeActivity: 0,
         activityList: [
             { //activity
                 id: 0,
@@ -116,35 +39,35 @@ var activityList = new Vue({
                 tags: ['easy', 'life-goals'],
                 custom:{}
             }
-        ]
+        ],
     },
     methods: {
-    }
-});
+        startTime: function(){
+            console.log('start');
+            this.clock = window.setInterval(this.updateTime, 100);
+            this.inProgress = true;
+        },
+        stopTime: function(){
+            console.log('stop');
+            clearInterval(this.clock);
+            this.inProgress = false;
+        },
+        updateTime: function(){
+            if(this.time.hour <= 0 && this.time.minute <= 0 && this.time.second <= 0){
+                clearInterval(this.clock);
+                //TODO: add start next activity if others exist or stop everything
+            }
 
-function getTime(value){
-    switch(value){
-        case 0:
-            return "00:30";
-        case 1:
-            return "05:30";
-        case 2:
-            return "02:15";
-        case 3:
-            return "00:45";
-        default:
-            return "00:15";
-    }
-}
-
-var app5 = new Vue({
-    // el: '#app-5',
-    data: {
-        message: "Kayak on kayak Mountain."
-    },
-    methods: {
-        reversemessage: function(){
-            this.message = this.message.split('').reverse().join('');
+            if(this.time.second > 0){
+                this.time.second--;
+            }else if(this.time.minute > 0){
+                this.time.minute--;
+                this.time.second = 59;
+            }else if(this.time.hour > 0){
+                this.time.hour--;
+                this.time.minute = 59;
+                this.time.second = 59;
+            }
         }
     }
 });
