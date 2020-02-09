e8073e347215e70bb48cac62d15b8c28ee24b93b
Kristian Bonitz <kristian@bonitz.me>
2020-02-10 06:57:02 +1100

    Moving Create and Delete tricks to config
    Trying to wrangle everything into more order

--- config/config.js
+++ config/config.js
@@ -13,22 +13,56 @@ pool.query('CREATE TABLE IF NOT EXISTS activities( id serial PRIMARY KEY, user_i
     console.log(err, res)
 });
 
-function getData(){
+module.exports.log = function(msg){
+    console.log(msg);
+    return 'Not again...'
+};
+
+module.exports.getData = new Promise( function(res, err){
     pool.query('SELECT * FROM activities', (err, result) => {
         if( err ){
             throw err;
         }
-        return result.rows;
+        res(result.rows);
     })
-}
+});
 
-module.exports = {
-    getData : function(){
-        pool.query('SELECT * FROM activities', (err, result) => {
+module.exports.createTrick = function(trick) {
+    if (!trick){
+        console.error("object is null");
+    }else{
+        text = "INSERT INTO activities (user_id,name,object_name,object_count,difficulty_grade,learning_stage,siteswap,description) VALUES ($1,$2,$3,$4,$5,$6,$7,$8)";
+        values = [1, 
+        trick.name ? trick.name : Error('trick name value is null') , 
+        trick.objectName ? trick.objectName : null, 
+        trick.objectCount ? trick.objectCount : null, 
+        trick.difficulty ? trick.difficulty : null, 
+        0,
+        trick.siteswap ? trick.siteswap : null, 
+        '' ];
+        pool.query(text, values, (err, result) => {
             if( err ){
                 throw err;
             }
+            console.log(result);
             return result.rows;
-        })
+        }) 
+    }
+};
+
+module.exports.deleteTrick = function deleteTrick(trickID) {
+    console.log("deleting trick " + trickID);
+    if (!trickID){
+        console.error("object is null");
+    }else{
+        text = "DELETE FROM activities WHERE id = $1";
+        values = [trickID];
+        pool.query(text, values, (err, result) => {
+            if( err ){
+                throw err;
+            }
+            return result;
+        }) 
     }
 }
+
--- server.js
+++ server.js
@@ -2,78 +2,25 @@ var express = require('express');
 var path = require('path'); 
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const { Pool, Client } = require('pg');
+const config = require('./config/config.js');
 var app = express();
 const port = 5432;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+app.use(express.static("./"));
 
 app.listen(port, () => console.log("App listening on port: ", port));
 
-const pool = new Pool({
-    user: 'clubsadmin',
-    host: 'localhost',
-    database: 'clubs',
-    password: 'iAWyQ2v3FZp',
-    port: 4321,
-});
-
-pool.query('CREATE TABLE IF NOT EXISTS activities( id serial PRIMARY KEY, user_id VARCHAR(100), name VARCHAR(100) NOT NULL,  object_name VARCHAR(100), object_count INT, difficulty_grade INT, learning_stage INT, siteswap VARCHAR(100))',
-    (err, res) => {
-    console.log(err, res)
-});
-
-function createTrick(trick) {
-    console.log(trick);
-    if (!trick){
-        console.error("object is null");
-    }else{
-        text = "INSERT INTO activities (user_id,name,object_name,object_count,difficulty_grade,learning_stage,siteswap,description) VALUES ($1,$2,$3,$4,$5,$6,$7,$8)";
-        values = [1, 
-        trick.name ? trick.name : Error('trick name value is null') , 
-        trick.objectName ? trick.objectName : null, 
-        trick.objectCount ? trick.objectCount : null, 
-        trick.difficulty ? trick.difficulty : null, 
-        0,
-        trick.siteswap ? trick.siteswap : null, 
-        '' ];
-        pool.query(text, values, (err, result) => {
-            if( err ){
-                throw err;
-            }
-            return result.rows;
-        }) 
-    }
-}
-
-function deleteTrick(trickID) {
-    console.log("deleting trick " + trickID);
-    if (!trickID){
-        console.error("object is null");
-    }else{
-        text = "DELETE FROM activities WHERE id = $1";
-        values = [trickID];
-        pool.query(text, values, (err, result) => {
-            if( err ){
-                throw err;
-            }
-            return result;
-        }) 
-    }
-}
+console.log(config);
+console.log(config.log("something?"));
 
 app.get('/getData', (req, res) => {
-    pool.query('SELECT * FROM activities', (err, result) => {
-        if( err ){
-            throw err;
-        }
-        res.status(200).json(result.rows);
+    config.getData.then(output => {
+        res.status(200).json(output);
     })
 });
-
-app.use(express.static("./"));
 
 app.get('/', (req, res) => {
     var options = {
@@ -118,14 +65,14 @@ app.get('/list', (req, res) => {
 });
 
 app.post('/addTrick', (req, res) => {
-    console.log('# add trick - post request recieved')
-    output = createTrick(req.body)
-    res.redirect('\list')
+    console.log('# add trick - post request recieved');
+    output = config.createTrick(req.body);
+    res.redirect('\list');
 });
 
 app.post('/deleteTrick', (req, res) => {
-    console.log('# delete trick - post request recieved')
-    output = deleteTrick(req.body.id)
+    console.log('# delete trick - post request recieved');
+    output = config.deleteTrick(req.body.id);
 });
 
 
